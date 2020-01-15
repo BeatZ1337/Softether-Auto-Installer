69fedc3881567f12d529b1c259ff658cf5f64137
BeatZVPN <59895507+BeatZVPN@users.noreply.github.com>
2020-01-14 20:37:57 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,90 +1,39 @@
-#!/bin/sh
-
-printf "\033[1;95mThis script will auto install SoftEther And Do Everything For You\033[1;97m \n";
-printf "\033[1;96mBeatZVPN ( :\033[1;97m \n";
-sleep 5;
-
-apt-get update -y >/dev/null;
-
-printf "\033[1;92mUpdate Done.\033[1;97m \n";
-sleep 5;
-
-apt-get remove apt-listchanges -y >/dev/null;
-apt-get upgrade -y >/dev/null;
-
-printf "\033[1;92mUpgrade Done.\033[1;97m \n";
-sleep 5;
-
-apt-get install checkinstall -y >/dev/null;
-apt-get install build-essential -y >/dev/null;
-
-printf "\033[1;92mDependencies Done.\033[1;97m \n";
-sleep 5;
-
-wget https://www.softether-download.com/files/softether/v4.24-9652-beta-2017.12.21-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.24-9652-beta-2017.12.21-linux-x64-64bit.tar.gz -o /dev/null;
-tar -xsf softether-vpnserver-v4.24-9652-beta-2017.12.21-linux-x64-64bit.tar.gz > /dev/null;
-rm -rf softether-vpnserver-v4.24-9652-beta-2017.12.21-linux-x64-64bit.tar.gz;
-
-printf "\033[1;92mFetched and Tar SoftEther.\033[1;97m \n";
-sleep 5;
-
-
-(cd vpnserver && (printf '1\n';sleep 1;printf '1\n';sleep 1;printf '1\n';sleep 1;) | make) >/dev/null
-printf "\033[1;92mOpenVPN Done.\033[1;97m \n";
-sleep 5;
-
-cd;
-mv vpnserver/ /usr/local;
-printf "\033[1;92mMoved Folder.\033[1;97m \n";
-sleep 5;
-chmod 600 * /usr/local/vpnserver/;
-chmod 700 /usr/local/vpnserver/vpnserver && chmod 700 /usr/local/vpnserver/vpncmd;
-
-printf "\033[1;92mPermissions Given.\033[1;97m \n";
-sleep 5;
-
-echo '#!/bin/sh
-# description: SoftEther VPN Server
-### BEGIN INIT INFO
-# Provides:          vpnserver
-# Required-Start:    $local_fs $network
-# Required-Stop:     $local_fs
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: softether vpnserver
-# Description:       softether vpnserver daemon
-### END INIT INFO
-DAEMON=/usr/local/vpnserver/vpnserver
-LOCK=/var/lock/subsys/vpnserver
-test -x $DAEMON || exit 0
-case "$1" in
-start)
-$DAEMON start
-touch $LOCK
-;;
-stop)
-$DAEMON stop
-rm $LOCK
-;;
-restart)
-$DAEMON stop
-sleep 3
-$DAEMON start
-;;
-*)
-echo "Usage: $0 {start|stop|restart}"
-exit 1
-esac
-exit 0' > /etc/init.d/vpnserver;
-printf "\033[1;92mScript Ran.\033[1;97m \n";
-sleep 5;
-
-chmod 755 /etc/init.d/vpnserver;
-update-rc.d vpnserver defaults;
-
-printf "\033[1;92mIt will start on boot.\033[1;97m \n";
-sleep 5;
-
-/etc/init.d/vpnserver start;
-printf "\033[32;5mSoftether Installed Sucessfully By BeatZ.\033[0m \n";
-
+#--------------------------------------------------
+#Softether auto install script
+#Debian and Ubuntu
+#--------------------------------------------------
+cd /root
+apt-get update -y
+apt-get install build-essential -y
+#Downloading server files
+wget http://www.softether-download.com/files/softether/v4.32-9731-beta-2020.01.01-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.32-9731-beta-2020.01.01-linux-x64-64bit.tar.gz
+tar zxf softether-vpnserver-v4.32-9731-beta-2020.01.01-linux-x64-64bit.tar.gz
+cd vpnserver
+#Installing server files, Manual input
+clear
+echo  -e "\033[31;7mNOTE: ANSWER 1 AND ENTER THREE TIMES FOR THE COMPILATION TO PROCEED\033[0m"
+make
+cd /root
+mv vpnserver /usr/local
+rm -rf softether-vpnserver-v4.32-9731-beta-2020.01.01-linux-x64-64bit.tar.gz
+cd /usr/local/vpnserver
+chmod 600 *
+chmod 700 vpncmd
+chmod 700 vpnserver
+#Installing server command
+wget https://raw.githubusercontent.com/Anonghost720/SoftEther-Autoinstal-Ubuntu-Debian/master/vpn-server.sh --no-check-certificate
+mv vpn-server.sh /etc/init.d/vpnserver
+cd /etc/init.d/
+chmod 755 vpnserver
+update-rc.d vpnserver defaults
+/etc/init.d/vpnserver start
+cd /usr/local/vpnserver
+echo ---------------------------------------------
+echo  -e "\033[32;5mSoftether Was Successfully Installed By BeatZ.\033[0m"
+echo "vpncmd is at /usr/local/vpnserver"
+echo ---------------Commands----------------------
+echo /etc/init.d/vpnserver start - to start
+echo /etc/init.d/vpnserver restart - to restart
+echo /etc/init.d/vpnserver stop - to stop
+echo ---------------------------------------------
+#End of script
